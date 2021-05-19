657dc54490f9d72157cfa8cd8509f87f792963b5
SantiagoVA <sanvara2006@gmail.com>
2021-05-19 15:23:30 -0500

    .env util

---
+++ utils/getEnv.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetEnv(key string) string {
+	err := godotenv.Load()
+
+	CheckErrors(err, "1", "Error loading the .env file", "Create a .env file and write there the value, something like this:\nVALUE_EXAMPLE=ThisIsTheValue")
+
+	value := os.Getenv(key)
+
+	return value
+}
