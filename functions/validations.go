900efb2999ba90106af321bbdf412d702e631b10
SantiagoVA <sanvara2006@gmail.com>
2021-05-19 22:23:38 -0500

    Feat: New routes to update and delete package's

---
+++ functions/validations.go
@@ -0,0 +1,38 @@
+package functions
+
+import (
+	"context"
+	"moldy-api/database"
+	"moldy-api/models"
+	"moldy-api/utils"
+	"strings"
+
+	"github.com/alexedwards/argon2id"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var packageCollection = database.GetCollection("packages")
+
+func RepeatedData(name string) bool {
+	var structure models.Package
+	err := packageCollection.FindOne(context.TODO(), primitive.D{{Key: "Name", Value: name}}).Decode(&structure)
+
+	if err != nil {
+		return false
+	}
+
+	return strings.EqualFold(structure.Name, name)
+}
+
+func SamePassword(password, id string) bool {
+	var structure models.Package
+	err := packageCollection.FindOne(context.TODO(), primitive.D{{Key: "id", Value: id}}).Decode(&structure)
+
+	utils.CheckErrors(err, "code 4", "The package not was found", "Verify the ID provided")
+
+	match, err := argon2id.ComparePasswordAndHash(password, structure.Password)
+
+	utils.CheckErrors(err, "code 5", "Unknown error validating the password", "Unknown solution")
+
+	return match
+}
