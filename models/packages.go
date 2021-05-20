cbae7ac0b09390873ecebc29085659ccdcd02535
SantiagoVA <sanvara2006@gmail.com>
2021-05-19 20:14:29 -0500

    Update: New blanks in the model of Package

--- models/packages.go
+++ models/packages.go
@@ -5,6 +5,15 @@ type Package struct {
 	Author      string `json:"author" bson:"author"`
 	Url         string `json:"url" bson:"url"`
 	Description string `json:"description" bson:"description"`
+	Password    string `json:"password" bson:"password"`
+	Version     string `json:"version" bson:"version"`
+}
+
+type Format struct {
+	Name        string `json:"name" bson:"name"`
+	Author      string `json:"author" bson:"author"`
+	Url         string `json:"url" bson:"url"`
+	Description string `json:"description" bson:"description"`
 	Version     string `json:"version" bson:"version"`
 }
 
