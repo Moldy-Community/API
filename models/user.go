405d15b0272495cefc3da6a1b9e95bb8be08d5a7
SantiagoVA <sanvara2006@gmail.com>
2021-07-08 14:51:06 -0500

    Feat: New model

--- models/user.go
+++ models/user.go
@@ -10,3 +10,10 @@ type Login struct {
 	Email    string `json:"email" bson:"email"`
 	Password string `json:"password" bson:"password"`
 }
+
+type UpdateUser struct {
+	Name        string `json:"name" bson:"name"`
+	Email       string `json:"email" bson:"email"`
+	OldPassword string `json:"oldpassword" bson:"oldpassword"`
+	NewPassword string `json:"newpassword" bson:"newpassword"`
+}
