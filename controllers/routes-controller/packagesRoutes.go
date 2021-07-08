30ef284039f050896a2fb3f688379ec154e432d4
SantiagoVA <sanvara2006@gmail.com>
2021-07-08 14:49:08 -0500

    Update: 2 functions deleted and added some validations of the tokens

--- controllers/routes-controller/packagesRoutes.go
+++ controllers/routes-controller/packagesRoutes.go
@@ -9,6 +9,7 @@ import (
 	"moldy-api/utils"
 	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -20,60 +21,50 @@ import (
 
 var packageCollection = database.GetCollection("packages")
 
-func GetAll(c *gin.Context) {
-	var allData models.Packages
-
-	cursor, err := packageCollection.Find(context.TODO(), primitive.D{})
-
-	utils.CheckErrors(err, "code 4", "Search finished", "No solution. The search finish")
-
-	for cursor.Next(context.Background()) {
-		var pkg models.Format
-		err = cursor.Decode(&pkg)
-
-		utils.CheckErrors(err, "code 4", "Search finished", "No solution. The search finish")
-
-		allData = append(allData, &pkg)
-	}
-
-	c.JSON(200, gin.H{
-		"error":   false,
-		"message": "Success search",
-		"data":    allData,
-	})
-
-}
-
 func SearchId(c *gin.Context) {
 	var structure models.Format
 	id := c.Param("id")
+	token := c.Request.Header.Get("token")
+
+	newToken := functions.NewToken(token)
 
 	err := packageCollection.FindOne(context.TODO(), primitive.D{{Key: "id", Value: id}}).Decode(&structure)
 
 	if err != nil {
 		c.JSON(404, gin.H{
-			"error":   true,
-			"message": "The package not was found by this ID, please verify if is correct.",
-			"data":    nil,
+			"error":    true,
+			"message":  "The package not was found by this ID, please verify if is correct.",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
 
 	c.JSON(200, gin.H{
-		"error":   false,
-		"message": "Success search by ID",
-		"data":    structure,
+		"error":    false,
+		"message":  "Success search by ID",
+		"data":     structure,
+		"newToken": newToken,
 	})
 }
 
 func SearchMany(c *gin.Context) {
-	name, found := c.GetQuery("key")
+	name, found := c.GetQuery("query")
+	limitStr, _ := c.GetQuery("limit")
+	limit, err := strconv.ParseInt(limitStr, 10, 64)
+	if limit == 0 || err != nil {
+		limit = 20
+	}
+	token := c.Request.Header.Get("token")
+
+	newToken := functions.NewToken(token)
 
 	if !found {
 		c.JSON(422, gin.H{
-			"error":   true,
-			"message": "Bad request, please provide a query param",
-			"data":    nil,
+			"error":    true,
+			"message":  "Bad request, please provide a query param",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
@@ -84,65 +75,41 @@ func SearchMany(c *gin.Context) {
 
 	utils.CheckErrors(err, "code 4", "Search finished", "No solution. The search finish")
 
-	for cursor.Next(context.Background()) {
-		var pkg models.Format
-		err = cursor.Decode(&pkg)
+	var i int64
 
-		utils.CheckErrors(err, "code 4", "Search finished", "No solution. The search finish")
+	for i = 0; cursor.Next(context.Background()); i++ {
+		if i < limit {
+			var pkg models.Format
+			_ = cursor.Decode(&pkg)
 
-		allData = append(allData, &pkg)
+			allData = append(allData, &pkg)
+
+		} else {
+			break
+		}
 	}
 
 	c.JSON(200, gin.H{
-		"error":   false,
-		"message": "Success search",
-		"data":    allData,
-	})
-
-}
-
-func SearchOne(c *gin.Context) {
-	name, found := c.GetQuery("key")
-
-	if !found {
-		c.JSON(422, gin.H{
-			"error":   true,
-			"message": "Bad request, please provide a query param",
-			"data":    nil,
-		})
-		return
-	}
-
-	var structure models.Format
-
-	err := packageCollection.FindOne(context.TODO(), primitive.D{{Key: "name", Value: name}}).Decode(&structure)
-
-	utils.CheckErrors(err, "code 4", "Not found any", "No solution. The search finish")
-	if err != nil {
-		c.JSON(404, gin.H{
-			"error":   true,
-			"message": "Package not found",
-			"data":    nil,
-		})
-		return
-	}
-
-	c.JSON(200, gin.H{
-		"error":   false,
-		"message": "Success search",
-		"data":    structure,
+		"error":    false,
+		"message":  "Success search",
+		"data":     allData,
+		"newToken": newToken,
 	})
 
 }
 
 func NewPackage(c *gin.Context) {
 	var reqBody models.Package
+	token := c.Request.Header.Get("token")
+
+	newToken := functions.NewToken(token)
 
 	if err := c.ShouldBindJSON(&reqBody); err != nil {
 		c.JSON(422, gin.H{
-			"error":   true,
-			"message": "The request was invalid",
-			"data":    nil,
+			"error":    true,
+			"message":  "The request was invalid",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
@@ -151,9 +118,10 @@ func NewPackage(c *gin.Context) {
 
 	if repeatedName {
 		c.JSON(409, gin.H{
-			"error":   true,
-			"message": "The name of this package was used before",
-			"data":    nil,
+			"error":    true,
+			"message":  "The name of this package was used before",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
@@ -164,43 +132,48 @@ func NewPackage(c *gin.Context) {
 
 	if invalid || strings.HasPrefix(reqBody.Version, ".") {
 		c.JSON(406, gin.H{
-			"error":   true,
-			"message": "Please provide a valid version (Only numbers and dot's)",
-			"data":    nil,
+			"error":    true,
+			"message":  "Please provide a valid version (Only numbers and dot's)",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
 
 	if len(reqBody.Version) > 5 {
 		c.JSON(411, gin.H{
-			"error":   true,
-			"message": "The length of the blank <version> is too long",
-			"data":    nil,
+			"error":    true,
+			"message":  "The length of the blank <version> is too long",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	} else if len(reqBody.Version) < 3 {
 		c.JSON(411, gin.H{
-			"error":   true,
-			"message": "The version is too short, remind that the format is X.Y or X.Y.Z",
-			"data":    nil,
+			"error":    true,
+			"message":  "The version is too short, remind that the format is X.Y or X.Y.Z",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
 
 	if reqBody.Description == "" || reqBody.Name == "" || reqBody.Url == "" {
 		c.JSON(411, gin.H{
-			"error":   true,
-			"message": "Please fill all blanks",
-			"data":    nil,
+			"error":    true,
+			"message":  "Please fill all blanks",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
 
 	if len(reqBody.Description) >= 150 {
 		c.JSON(411, gin.H{
-			"error":   true,
-			"message": "The description is too long and have more of 150 characters, please write it more short",
-			"data":    nil,
+			"error":    true,
+			"message":  "The description is too long and have more of 150 characters, please write it more short",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
@@ -209,15 +182,16 @@ func NewPackage(c *gin.Context) {
 
 	if err != nil || u.Host == "" || u.Scheme == "" {
 		c.JSON(406, gin.H{
-			"error":   true,
-			"message": "The URL is not valid",
+			"error":    true,
+			"message":  "The URL is not valid",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
 
 	reqBody.ID = uuid.New().String()
 
-	token := c.Request.Header.Get("token")
 	validatedToken, _ := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
@@ -232,9 +206,10 @@ func NewPackage(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(500, gin.H{
-			"error":   true,
-			"message": "Something bad happened when the data was saved",
-			"data":    nil,
+			"error":    true,
+			"message":  "Something bad happened when the data was saved",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
@@ -249,28 +224,31 @@ func NewPackage(c *gin.Context) {
 	}
 
 	c.JSON(200, gin.H{
-		"error":   false,
-		"message": "Created successfully",
-		"data":    formated,
+		"error":    false,
+		"message":  "Created successfully",
+		"data":     formated,
+		"newToken": newToken,
 	})
 
 }
 
 func UpdatePackage(c *gin.Context) {
 	id := c.Param("id")
-
 	var reqBody models.PackageUpdate
+	token := c.Request.Header.Get("token")
+
+	newToken := functions.NewToken(token)
 
 	if err := c.ShouldBindJSON(&reqBody); err != nil {
 		c.JSON(422, gin.H{
-			"error":   true,
-			"message": "The request was invalid",
-			"data":    nil,
+			"error":    true,
+			"message":  "The request was invalid",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
 
-	token := c.Request.Header.Get("token")
 	validatedToken, _ := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
@@ -297,17 +275,19 @@ func UpdatePackage(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(500, gin.H{
-			"error":   true,
-			"message": "Something bad happened when the data was attempted to save",
-			"data":    nil,
+			"error":    true,
+			"message":  "Something bad happened when the data was attempted to save",
+			"data":     nil,
+			"newToken": newToken,
 		})
 	}
 
 	if err != nil {
 		c.JSON(500, gin.H{
-			"error":   true,
-			"message": "The package cannot be updated for some unknown reason",
-			"data":    nil,
+			"error":    true,
+			"message":  "The package cannot be updated for some unknown reason",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
@@ -323,16 +303,19 @@ func UpdatePackage(c *gin.Context) {
 			Description: reqBody.Description,
 			Version:     reqBody.Version,
 		},
+		"newToken": newToken,
 	})
 }
 
 func DeleteOne(c *gin.Context) {
 	var structure models.Package
 	id := c.Param("id")
-
-	err := userCollection.FindOne(context.TODO(), primitive.D{{Key: "id", Value: id}}).Decode(&structure)
-
 	token := c.Request.Header.Get("token")
+
+	newToken := functions.NewToken(token)
+
+	err := packageCollection.FindOne(context.TODO(), primitive.D{{Key: "id", Value: id}}).Decode(&structure)
+
 	validatedToken, _ := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
@@ -344,9 +327,10 @@ func DeleteOne(c *gin.Context) {
 
 	if structure.Author != string(claims["name"].(string)) {
 		c.JSON(403, gin.H{
-			"error":   true,
-			"message": "Unauthorized",
-			"data":    nil,
+			"error":    true,
+			"message":  "Unauthorized",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
@@ -355,16 +339,18 @@ func DeleteOne(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(500, gin.H{
-			"error":   true,
-			"message": "Something bad happen, the package was cannot deleted",
-			"data":    nil,
+			"error":    true,
+			"message":  "Something bad happen, the package was cannot deleted",
+			"data":     nil,
+			"newToken": newToken,
 		})
 		return
 	}
 
 	c.JSON(200, gin.H{
-		"error":   false,
-		"message": "The package with the id " + id + " was deleted successfully",
-		"data":    nil,
+		"error":    false,
+		"message":  "The package with the id " + id + " was deleted successfully",
+		"data":     nil,
+		"newToken": newToken,
 	})
 }
